d909c27ee73e035c373952877bd998551cc55e82
Javier Junquera Sáncez <javier.junquera.sanchez@gmail.com>
2017-10-22 22:54:55 +0200

    Update math_quiz.py

--- math_quiz.py
+++ math_quiz.py
@@ -16,6 +16,7 @@ def main():
         "Description:\n"
         "\n"
         "Runs a timed quiz of math equations based on a range from 1 to 10."
+        " "
         "User can specify the start and end of the range for both terms.\n"
     )
 
