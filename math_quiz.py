7c8d3f4bf31214c0206a0473ffcec36488b83990
James Candan <jcandan82@gmail.com>
2017-10-22 11:08:54 -0400

    make addition quiz for all 1-10.

--- math_quiz.py
+++ math_quiz.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+import time
+import random
+import argparse
+
+
+def main():
+    questions = []
+    time_0 = time.time()
+
+    factors_1 = list(reversed(range(1, 10 + 1)))
+    factors_2 = list(reversed(range(1, 10 + 1)))
+    for factor_1 in factors_1:
+        for factor_2 in factors_2:
+            questions.append({'one': factor_1, 'two': factor_2})
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--random", help = "Randomize set of questions.", action = "store_true")
+    args = parser.parse_args()
+
+    if args.random:
+        random.shuffle(questions)
+
+    for question in questions:
+        correct = question['one'] + question['two']
+        user_response = input(str(question['one']) + " + " + str(question['two']) + " = ")
+        if str(correct) != user_response:
+            print("Incorrect :( . . . The answer is " + str(correct))
+
+    time_delta = time.time() - time_0
+    m, s = divmod(time_delta, 60)
+    print('Time: %02d:%02d' % (m, s))
+
+
+if __name__ == "__main__":
+    main()
