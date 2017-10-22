fa8bf9a8813e5c94ee83eda97bf2a2bebe873a58
James Candan <jcandan82@gmail.com>
2017-10-22 13:49:15 -0400

    refer to first and second term, not one and two.

--- math_quiz.py
+++ math_quiz.py
@@ -11,28 +11,28 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--random", help = "Randomize set of questions.", action = "store_true")
-    parser.add_argument("-s1", "--start_one", help = "Specify start of range for first term",
+    parser.add_argument("-s1", "--start_first", help = "Specify start of range for first term",
                         type = int, default = 1)
-    parser.add_argument("-e1", "--end_one", help = "Specify end of range for first term",
+    parser.add_argument("-e1", "--end_first", help = "Specify end of range for first term",
                         type = int, default = 10)
-    parser.add_argument("-s2", "--start_two", help = "Specify start of range for second term",
+    parser.add_argument("-s2", "--start_second", help = "Specify start of range for second term",
                         type = int, default = 1)
-    parser.add_argument("-e2", "--end_two", help = "Specify end of range for second term",
+    parser.add_argument("-e2", "--end_second", help = "Specify end of range for second term",
                         type = int, default = 10)
     args = parser.parse_args()
 
-    terms_1 = list(reversed(range(args.start_one, args.end_one + 1)))
-    terms_2 = list(reversed(range(args.start_two, args.end_two + 1)))
+    terms_1 = list(reversed(range(args.start_first, args.end_first + 1)))
+    terms_2 = list(reversed(range(args.start_second, args.end_second + 1)))
     for term_1 in terms_1:
         for term_2 in terms_2:
-            questions.append({'one': term_1, 'two': term_2})
+            questions.append({'first': term_1, 'second': term_2})
 
     if args.random:
         random.shuffle(questions)
 
     for question in questions:
-        correct = question['one'] + question['two']
-        user_response = input(str(question['one']) + " + " + str(question['two']) + " = ")
+        correct = question['first'] + question['second']
+        user_response = input(str(question['first']) + " + " + str(question['second']) + " = ")
         if str(correct) != user_response:
             print("Incorrect :( . . . The answer is " + str(correct))
 
